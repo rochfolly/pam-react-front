a8aff2270ea20b61bb5309cf9eff90a8fd32bc07
Roch <rochfolly@hotmail.fr>
2018-11-20 00:00:57 +0100

    db

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import Accueil from './Accueil/Accueil.js';
 
+/* var db = require('./db')
+
+app.use('/comments', require('./controllers/comments'))
+app.use('/users', require('./controllers/users'))
+
+// Connect to MySQL on start
+db.connect(db.MODE_PRODUCTION, function(err) {
+  if (err) {
+    console.log('Unable to connect to MySQL.')
+    process.exit(1)
+  } else {
+    app.listen(3000, function() {
+      console.log('Listening on port 3000...')
+    })
+  }
+}) */
 
 class App extends Component {
   render() {
