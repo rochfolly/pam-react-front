02b6853a59a51b7c93eea8ebd00c6a363163b645
Roch <rochfolly@hotmail.fr>
2018-11-20 13:44:10 +0100

    Connexion à la bdd

--- pam-node-api/serveur.js
+++ pam-node-api/serveur.js
@@ -1,42 +1,42 @@
-var http = require('http');
-var url = require('url');
-var mysql = require('mysql')
-var db = require('./db')
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const app = express();
 
-var server = http.createServer(function(req, res) {
-    var page = url.parse(req.url).pathname;
-    console.log(page);
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    if (page == '/') {
-        res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
-    }
-    else if (page == '/sous-sol') {
-        res.write('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
-    }
-    else if (page == '/etage/1/chambre') {
-        res.write('Hé ho, c\'est privé ici !');
-    }
-    else{
-      res.writeHead(404);
-      res.write('Erreur');
-    }
-    res.end();
-});
-server.listen(8080);
+const SELECT_ADMINS = 'SELECT name FROM admin';
 
-/* var connection = mysql.createConnection({
+ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'pam'
-})
+});
 
 connection.connect(function(err) {
   if (err) throw err
   console.log('You are now connected...')
 })
 
-connection.query('CREATE TABLE people(id int primary key, name varchar(255), age int, address text)', function(err, result) {
+app.get('/', (req, res) => {
+  res.send('go to /admins to see all the developpers')
+});
+
+app.get('/admins', (req, res) => {
+  connection.query(SELECT_ADMINS, (err, results) => {
+    if(err) {
+      return res.send(err)
+    }
+    else {
+      return res.json({
+        data: results
+      })
+    }
+  })
+});
+
+app.listen(8080)
+/*
+connection.query('CREATE TABLE people(id int primary key, name constchar(255), age int, address text)', function(err, result) {
     if (err) throw err
     connection.query('INSERT INTO people (name, age, address) VALUES (?, ?, ?)', ['Larry', '41', 'California, USA'], function(err, result) {
       if (err) throw err
@@ -51,19 +51,19 @@ connection.query('CREATE TABLE people(id int primary key, name varchar(255), age
   })
 })  */
 
-var data = {
-  tables: {
-    people: [
-     {id: 1, name: "John", age: 32},
-     {id: 2, name: "Peter", age: 29},
-    ],
-    cars: [
-      {id: 1, brand: "Jeep", model: "Cherokee", owner_id: 2},
-      {id: 2, brand: "BMW", model: "X5", owner_id: 2},
-      {id: 3, brand: "Volkswagen", model: "Polo", owner_id: 1},
-    ],
-  },
-}
+// const data = {
+//   tables: {
+//     people: [
+//      {id: 1, name: "John", age: 32},
+//      {id: 2, name: "Peter", age: 29},
+//     ],
+//     cars: [
+//       {id: 1, brand: "Jeep", model: "Cherokee", owner_id: 2},
+//       {id: 2, brand: "BMW", model: "X5", owner_id: 2},
+//       {id: 3, brand: "Volkswagen", model: "Polo", owner_id: 1},
+//     ],
+//   },
+// }
 
 /*db.connect(db.MODE_PRODUCTION, function() {
   db.fixtures(data, function(err) {
