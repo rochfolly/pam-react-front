bc5ab4f891e0d738175094c304c910ff19126520
colineJouy <coline.jouy@gmail.com>
2019-01-18 14:30:45 +0100

    fin affichage jeu image, reste à gerer les réponses

--- src/Jeux/JeuImage.js
+++ src/Jeux/JeuImage.js
@@ -3,44 +3,38 @@ import { Container, Row, Col, Button} from 'reactstrap';
 import './JeuImage.css'
 import game from '../Images/dice.png'
 import axios from 'axios';
-import styled from 'styled-components';
-
-class ColImg extends Component {
-    constructor(props){
-        super(props);
-     
-    }
-
-    render(){
-        const urlImg = "file:///"+__dirname+"../Images/images/1.jpg"
-        const ColImage = styled(Col) `background-image: url(${urlImg})`
-        console.log(__dirname)
-      return(
-        <ColImage sm={{size: 3, offset:3}} className="jeuImg"></ColImage>
-        
-      );}
-}
-
 
 class JeuImage extends Component {
 
     constructor(props){
         super(props);
-        this.state = {question:0, lien:'', word1:''}
+        this.state = {question:0, 
+            lien:'images/0.png',
+            word1:'', word2:'', word3:'', word4:'', word5:'',
+            reponse:''}
     }
     
     componentDidMount(){
         const images=["img_1","img_2","img_3","img_4","img_5"
             ,"img_6","img_7","img_8","img_9","img_10"]
-        const gameurl = "http://192.168.43.68/exo2"
+        const gameurl = "https://pfepamapi.azurewebsites.net/exo2"
         axios.post(gameurl)
         .then(res => {
             console.log(res.data[images[this.state.question]][0].src)
             console.log(res.data[images[this.state.question]][1])
             this.setState({ lien: res.data[images[this.state.question]][0].src,
-                 word1: res.data[images[this.state.question]][1].word_1 })
-            })          
+                 word1: res.data[images[this.state.question]][1].word_1,
+                 word2: res.data[images[this.state.question]][1].word_2,
+                 word3: res.data[images[this.state.question]][1].word_3,
+                 word4: res.data[images[this.state.question]][1].word_4,
+                 word5: res.data[images[this.state.question]][1].word_5 })
+            })   
+            console.log(this.state.lien)       
     }
+
+    // updateRep(rep){
+    //     this.setState({reponse: rep})
+    // }
 
   render() {
     
@@ -59,14 +53,15 @@ class JeuImage extends Component {
         <Col sm={{size: 1, offset:1}}><img src={game} alt="jeu" className="jeuImageLogo"/></Col>
         </Row>
         <Row>
-            <ColImg sm={{size: 3, offset:3}} lien={'../Images/images/1.jpg'} className="jeuImg" ref="bordel">
-            </ColImg>
+            <Col sm={{size: 3, offset:3}} className="jeuImg">
+                <img height="300" width="300" src={require(`./../Images/${this.state.lien}`)} className="imgExo"></img>
+            </Col>
             <Col sm={{size: 3, offset:1}}>
                 <Row><Button className="jeuSol" id="jeuSol1">{this.state.word1}</Button></Row>
-                <Row><Button className="jeuSol" id="jeuSol2">Tigre</Button></Row>
-                <Row><Button className="jeuSol" id="jeuSol3">Glace</Button></Row>
-                <Row><Button className="jeuSol" id="jeuSol4">Poisson</Button></Row>
-                <Row><Button className="jeuSol" id="jeuSol5">Guacamole</Button></Row>
+                <Row><Button className="jeuSol" id="jeuSol2">{this.state.word2}</Button></Row>
+                <Row><Button className="jeuSol" id="jeuSol3">{this.state.word3}</Button></Row>
+                <Row><Button className="jeuSol" id="jeuSol4">{this.state.word4}</Button></Row>
+                <Row><Button className="jeuSol" id="jeuSol5">{this.state.word5}</Button></Row>
             </Col>
         </Row>
         <Row>
