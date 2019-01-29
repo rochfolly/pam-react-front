2135426d9785d281af5b31fa07e3caab2c9a733a
colineJouy <coline.jouy@gmail.com>
2019-01-29 12:12:32 +0100

    Affichage des résultats sous forme de tableau

--- src/Jeux/Result.js
+++ src/Jeux/Result.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Button} from 'reactstrap';
+import { Container, Row, Col, Button, Table} from 'reactstrap';
 import './Result.css'
 import trophy from '../Images/trophy.png'
 
@@ -35,67 +35,77 @@ class Result extends Component {
 
     createQuestion(title, i){
         if (title === "Texte à trou") {
-        return [<h4>Phrase à trou {i}</h4>];
-        //return [<h4>{this.state.reponse[i].part1} ______ {this.state.reponse[i].part2}</h4>]
+        //return [<h4>Phrase à trou {i}</h4>];
+        return [<h5>{this.state.reponse[i].part1} ______ {this.state.reponse[i].part2}</h5>]
         }
         else if (title === "Jeu d'image") {
-        return [<h4>Image {i}</h4>];
-        //return [<img height="50" width="50" alt="" src={require(`./../Images/${this.state.reponse[i].src}`)} className="imgExo"></img>]
+        //return [<h4>Image {i}</h4>];
+        return [<img height="100" width="100" alt="" style={{objectFit:"cover"}} src={require(`./../Images/${this.state.reponse[i].src}`)}></img>]
         }
         else if (title === "La bonne image") {
-            return [<h4>Label {i}</h4>];
-        //return [<h4>{this.state.reponse[i].label}</h4>]
+        //    return [<h4>Label {i}</h4>];
+        return [<h3>{this.state.reponse[i].label}</h3>]
         }
     }
 
     createResponse(title, i){
         if (title === "Texte à trou") {
-        return [<h4>Réponse user</h4>];
-        //return [<h4>{this.state.reponse[i].repU}</h4>]
+        //return [<h4>Réponse user</h4>];
+        return [<h5>{this.state.reponse[i].repu}</h5>]
         }
         else if (title === "Jeu d'image") {
-        return [<h4>Réponse user</h4>];
-        //return [<h4>{this.state.reponse[i].repU}</h4>]
+        //return [<h4>Réponse user</h4>];
+        return [<h3>{this.state.reponse[i].repU}</h3>]
         }
         else if (title === "La bonne image") {
-            return [<h4>Image user</h4>];
-        //return [<img height="50" width="50" alt="" src={require(`./../Images/${this.state.reponse[i].repU}`)} className="imgExo"></img>]
+        //    return [<h4>Image user</h4>];
+        return [<img height="100" width="100" alt="" style={{objectFit:"cover"}} src={require(`./../Images/${this.state.reponse[i].repU}`)}></img>]
         }
     }
 
     createResult(title, i){
         if (title === "Texte à trou") {
-        return [<h4>Réponse à donner</h4>];
-        //return [<h4>{this.state.reponse[i].repT}</h4>]
+        //return [<h4>Réponse à donner</h4>];
+        return [<h5>{this.state.reponse[i].rept}</h5>]
         }
         else if (title === "Jeu d'image") {
-        return [<h4>Réponse à donner</h4>];
-        //return [<h4>{this.state.reponse[i].repT}</h4>]
+        //return [<h4>Réponse à donner</h4>];
+        return [<h3>{this.state.reponse[i].repT}</h3>]
         }
         else if (title === "La bonne image") {
-        return [<h4>Image à donner</h4>];
-        //return [<img height="50" width="50" alt="" src={require(`./../Images/${this.state.reponse[i].repT}`)} className="imgExo"></img>]
+        //return [<h4>Image à donner</h4>];
+        return [<img height="100" width="100" alt="" style={{objectFit:"cover"}} src={require(`./../Images/${this.state.reponse[i].repT}`)}></img>]
         }
     }
 
     render() {
 
-        const final = (this.state.result) ? this.state.result : 'Nope'
+        const final = (this.state.result) ? this.state.result : '0'
         //display: this.isLastLevel() ? "none": "block"
 
         var indents = [];
 
         for (var i = 0; i < score.length; i++) { //-1 car une ligne pr le titre de l'exo
-        indents.push(<Row key={i}>
-            <h3>Exercice {i+1} : {this.state.reponse[i].score}/100</h3></Row>);
-        indents.push(<Row key={i}>
-            {this.createQuestion(this.state.title, i)}</Row>);
-        indents.push(<Row key={i}>
-            {this.createResponse(this.state.title, i)}</Row>);
-        indents.push(<Row key={i}>
-            {this.createResult(this.state.title, i)}</Row>);
-        indents.push(<br/>);
+        // indents.push(<Row key={i}>
+        //     <h3>Exercice {i+1} : {this.state.reponse[i].score}/100</h3></Row>);
+        // indents.push(<Row key={i}>
+        //     {this.createQuestion(this.state.title, i)}</Row>);
+        // indents.push(<Row key={i}>
+        //     {this.createResponse(this.state.title, i)}</Row>);
+        // indents.push(<Row key={i}>
+        //     {this.createResult(this.state.title, i)}</Row>);
+        // indents.push(<br/>);
+
+        indents.push(
+        <tr>
+            <th style={{verticalAlign:"middle"}} scope="row"><h4 className="titlePAM">{i+1}.</h4></th>
+            <td style={{verticalAlign:"middle"}}>{this.createQuestion(this.state.title, i)}</td>
+            <td style={{verticalAlign:"middle"}}>{this.createResponse(this.state.title, i)}</td>
+            <td style={{verticalAlign:"middle"}}>{this.createResult(this.state.title, i)}</td>
+            <td style={{verticalAlign:"middle"}}><h4>{this.state.reponse[i].score}</h4></td>
+        </tr>);
         }
+
 
         return (
             <Container>
@@ -112,16 +122,35 @@ class Result extends Component {
                 <Col sm={{size: 1}}><Button className ="smallButton"><h2><i className="fa fa-power-off"></i></h2></Button></Col>
                 </Row>
                 <Row>
-                    <Col sm={{size:8, offset:1}}>
+                    <Col sm={{size:10, offset:1}}>
+                    <Table borderless>
+                    <thead>
+                    <tr>
+                        <th><h4>Question</h4></th>
+                        <th><h4>Consigne</h4></th>
+                        <th><h4>Votre réponse</h4></th>
+                        <th><h4>La bonne réponse</h4></th>
+                        <th><h4>Score</h4></th>
+                    </tr>
+                    </thead>
+                    <tbody>
                     {indents}
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><h4 className="titlePAM">Total :</h4></td>
+                        <td><h2 className="titlePAM">{final}</h2></td>
+                    </tr>
+                    </tbody>
+                    </Table>
                     <br/>
                     </Col>
-                    <Col sm="3">
-                        <br/><br/><br/>
-                    <Row>
-                        <h2 className="titlePAM">Total : {final}</h2></Row>
-                    </Col>
                 </Row>
+                {/* <Row>
+                    <h2 className="titlePAM">Total : {final}</h2>
+                </Row> */}
+                    
             </Container>
         );
     }
