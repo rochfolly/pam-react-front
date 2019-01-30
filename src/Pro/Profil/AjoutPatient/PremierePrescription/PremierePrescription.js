6403b2961017ff15873174f82114d5ef7ceeba82
colineJouy <coline.jouy@gmail.com>
2019-01-30 16:36:01 +0100

    mise en page première prescription

--- src/Pro/Profil/AjoutPatient/PremierePrescription/PremierePrescription.js
+++ src/Pro/Profil/AjoutPatient/PremierePrescription/PremierePrescription.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import { Container, Row, Form, Col, Button, Input, FormGroup, Label } from 'reactstrap';
-import game from '../../../../Images/dice.png';
+import { Container, Row, Form, Col, Button, Input, FormGroup} from 'reactstrap';
+import txtATrou from '../../../../Images/txtATrou.png';
+import bneImg from '../../../../Images/bneImg.png';
+import jeuImage from '../../../../Images/jeuImage.png';
 import '../../../../Jeux/JeuImage.css';
 import { showUser } from '../../../../utils/API'
+import './Prescription.css'
 
-class Checkbox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { checked: false, new_id:'', firstname:'', name:''};
-  }
+// class Checkbox extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { checked: false, new_id:'', firstname:'', name:''};
+//   }
 
-  handleCheck(){
-    this.setState({checked: !this.state.checked})
-  }
+//   handleCheck(){
+//     this.setState({checked: !this.state.checked})
+//   }
 
-  render(){
-    return(
-      <Input type="checkbox" value={this.props.value} onChange={this.handleCheck} />
-    )
+//   render(){
+//     return(
+//       <Input type="checkbox" value={this.props.value} onChange={this.handleCheck} />
+//     )
 
-  }
-}
+//   }
+// }
 
 class PremierePrescription extends Component {
   constructor(props) {
@@ -64,57 +67,95 @@ class PremierePrescription extends Component {
 
     return (
       <Container>
-        <Form onSubmit={this.handleSubmit}> 
+        <br/>
         <Row>
-        <Col sm={{size: 10}}><h3 class="titlePAM">Prescrption d'Exercices</h3></Col>
-        <Col sm={{size: 1}}><Button className ="smallButton"><a href="/profil"><h2><i class="fa fa-arrow-left"></i></h2></a></Button></Col>
-        <Col sm={{size: 1}}><Button className ="smallButton"><h2><i class="fa fa-power-off"></i></h2></Button></Col>
+        <Col sm={{size: 10}}><h3 className="titlePAM">Prescription d'exercices</h3></Col>
+        <Col sm={{size: 1}}><Button className ="smallButton"><a href="/profil"><h2><i className="fa fa-arrow-left"></i></h2></a></Button></Col>
+        <Col sm={{size: 1}}><Button className ="smallButton"><h2><i className="fa fa-power-off"></i></h2></Button></Col>
         </Row>
-          <FormGroup row>
-            <Col sm={3}><img src={game} alt="jeu" class="Game-logo"/></Col>
+        <br/><br/>
+        <Row>
+          <Col sm={{size:"8", offset:"2"}}>
+          <Form onSubmit={this.handleSubmit}> 
+            <hr/>
+            <FormGroup row>
+            <Input type="checkbox" checked={this.state.exos[0] === 1}/>
+            <Col sm={3}><img src={txtATrou} alt="jeu" class="Game-logo"/></Col>
             <Col sm={8}>
+            <Row>Exercice 1 : Texte à Trous</Row>
+            <br/>
             <Row>
-            <Col sm={3}><Input type="checkbox" checked={this.state.exos[0] === 1} /></Col>
-             <Col sm={9}><b>Exercice 1 : Texte à Trous</b></Col>
+              <small>Le joueur doit retrouver le mot manquant d'une phrase.<br/>
+              Au niveau 1 il a le choix entre 4 mots très différents,
+              niveau 2 entre 4 mots plus proches,
+              niveau 3 il doit retrouver seul le mot.</small>
             </Row>
             <br/>
             <Row>
-              <Col sm={3} className="no-padding">Niveau</Col>
-              <Col sm={2}>1<Input type="checkbox" value={1} id="niv1" onChange={this.handleChange_1} defaultChecked/> </Col>
-              <Col sm={2}>2<Input type="checkbox" value={2} id="niv2" onChange={this.handleChange_1}/></Col>
-              <Col sm={2}>3<Input type="checkbox" value={3} id="niv3" onChange={this.handleChange_1} /></Col>
-            </Row>
-            <br/>
-            <Row>
-             <Col sm={9}><b>Exercice 2 : Jeu d'Images</b></Col>
-            </Row>
-            <br/>
-            <Row>
-              <Col sm={3} className="no-padding">Niveau</Col>
-              <Col sm={2}>1<Input type="checkbox" value={1} id="niv1" onChange={this.handleChange_2} defaultChecked/> </Col>
-              <Col sm={2}>2<Input type="checkbox" value={2} id="niv2" onChange={this.handleChange_2}/></Col>
-              <Col sm={2}>3<Input type="checkbox" value={3} id="niv3" onChange={this.handleChange_2} /> </Col>
-            </Row>
-            <br/>
-            <Row>
-             <Col sm={9}><b>Exercice 3 : La Juste Image</b></Col>
-            </Row>
-            <br/>
-            <Row>
-              <Col sm={3} className="no-padding">Niveau</Col>
-              <Col sm={2}>1<Input type="checkbox" value={1} id="niv1" onChange={this.handleChange_3} defaultChecked/> </Col>
-              <Col sm={2}>2<Input type="checkbox" value={2} id="niv2" onChange={this.handleChange_3}/></Col>
-              <Col sm={2}>3<Input type="checkbox" value={3} id="niv3" onChange={this.handleChange_3} /></Col>
-            </Row>
-            <br/>
-            <Row>
-             <Col sm={{size:2, offset:10}}>
-              <Button type="submit">Valider</Button>
-             </Col>
+            <Col sm={3} className="no-padding">Niveau</Col>
+            <Col sm={2}>1<Input type="radio" name="exo1"  value={1} id="niv1" onChange={this.handleChange_1}/></Col>
+            <Col sm={2}>2<Input type="radio" name="exo1" value={2} id="niv2" onChange={this.handleChange_1}/></Col>
+            <Col sm={2}>3<Input type="radio" name="exo1" value={3} id="niv3" onChange={this.handleChange_1}/></Col>
             </Row>
             </Col>
-          </FormGroup>
-        </Form>
+            </FormGroup>
+            <hr/>
+
+            <FormGroup row>
+            <Input type="checkbox" checked={this.state.exos[0] === 1}/>
+            <Col sm={3}><img src={jeuImage} alt="jeu" class="Game-logo"/></Col>
+            <Col sm={8}>
+            <Row>Exercice 2 : Jeu d'Images</Row>
+            <br/>
+            <Row>
+              <small>Le joueur doit retrouver le mot correspondant à l'image affichée. <br/>
+              Au niveau 1 il a le choix entre 5 mots très différents,
+              niveau 2 entre 5 mots plus proches
+              niveau 3 il doit retrouver seul le mot.</small>
+            </Row>
+            <br/>
+            <Row>
+            <Col sm={3} className="no-padding">Niveau</Col>
+            <Col sm={2}>1<Input type="radio" name="exo2" value={1} id="niv1" onChange={this.handleChange_2}/></Col>
+            <Col sm={2}>2<Input type="radio" name="exo2" value={2} id="niv2" onChange={this.handleChange_2}/></Col>
+            <Col sm={2}>3<Input type="radio" name="exo2" value={3} id="niv3" onChange={this.handleChange_2}/></Col>
+            </Row>
+            </Col>
+            </FormGroup>
+            <hr/>
+
+            <FormGroup row>
+            <Input type="checkbox" checked={this.state.exos[0] === 1}/>
+            <Col sm={3}><img src={bneImg} alt="jeu" class="Game-logo"/></Col>
+            <Col sm={8}>
+            <Row>Exercice 3 : La bonne image</Row>
+            <br/>
+            <Row>
+              <small>Le joueur doit retrouver l'image correspondant au mot affiché.<br/>
+              Au niveau 1 il a le choix entre 2 photos,
+              au niveau 2 entre 4 photos.</small>
+            </Row>
+            <br/>
+            <Row>
+            <Col sm={3} className="no-padding">Niveau</Col>
+            <Col sm={2}>1<Input type="radio" name="exo3" value={1} id="niv1" onChange={this.handleChange_3}/></Col>
+            <Col sm={2}>2<Input type="radio" name="exo3" value={2} id="niv2" onChange={this.handleChange_3}/></Col>
+            </Row>
+            </Col>
+            </FormGroup>
+            <hr/>
+
+            <Row>
+            <Col sm={{size:2, offset:10}}>
+              <Button type="submit">Valider</Button>
+            </Col>
+            </Row>
+
+            </Form>
+          </Col>
+        </Row>
+        <br/>
+        
       </Container>
     );
   }
